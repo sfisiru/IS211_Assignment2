2c0e2e0824cecea1fc56b6baf0f376592e497332
Shehou Fisiru <fisirushehou@gmail.com>
2026-02-08 22:02:23 -0500

    Completed

--- assignment2.py
+++ assignment2.py
@@ -3,19 +3,68 @@ import urllib.request
 import logging
 import datetime
 
+
 def downloadData(url):
     """Downloads the data"""
-    pass
+    response = urllib.request.urlopen(url)
+    return response.read().decode("utf-8")
+
 
 def processData(file_content):
-    pass
+    logger = logging.getLogger("assignment2")
+    personData = {}
+
+    lines = file_content.splitlines()
+
+    for line_num, line in enumerate(lines, start=1):
+
+        if line.startswith("id"):
+            continue
+
+        try:
+            person_id, name, birthday_str = line.split(",")
+
+            birthday = datetime.datetime.strptime(birthday_str, "%d/%m/%Y")
+
+            personData[int(person_id)] = (name, birthday)
+
+        except Exception:
+            logger.error(f"Error processing line #{line_num} for ID #{person_id}")
+
+    return personData
 
 
 def displayPerson(id, personData):
-    pass
+    if id not in personData:
+        print("No user found with that id")
+    else:
+        name, birthday = personData[id]
+        print(f"Person #{id} is {name} with a birthday of {birthday.date()}")
+
 
 def main(url):
     print(f"Running main with URL = {url}...")
+
+    logging.basicConfig(filename="error.log", level=logging.ERROR)
+
+    try:
+        csvData = downloadData(url)
+    except Exception:
+        print("Error downloading data")
+        return
+
+    personData = processData(csvData)
+
+    while True:
+        try:
+            user_id = int(input("Enter an ID to lookup: "))
+        except ValueError:
+            continue
+
+        if user_id <= 0:
+            return
+
+        displayPerson(user_id, personData)
 
 
 if __name__ == "__main__":
